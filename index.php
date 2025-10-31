04d1e8a3ccab9a2ba982d4fbc5a1b0a96c275649
Rian Agustian <mrian3950@gmail.com>
2025-10-31 19:45:33 +0700

    Ganti jari-jari di pertemuan6

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('./lingkaran.php'); //panggil file lingkaran
 
 Use App\Math\LuasLingkaran;//panggil namespace
 
-$lingkaran = new LuasLingkaran(10);
+$lingkaran = new LuasLingkaran(15);
 $lingkaran->tampil('roda');
 LuasLingkaran::testing();
 
