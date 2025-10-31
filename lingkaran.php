c1f5ff22eaba18633f1e2cc24048fde0452a037e
Rian Agustian <mrian3950@gmail.com>
2025-10-31 20:29:45 +0700

    Mulai ulang pertemuan6 dari awal

--- lingkaran.php
+++ lingkaran.php
@@ -1,14 +1,32 @@
+
 <?php
 
-class LuasLingkaran{
+namespace App\Math; //namespace
+
+class LuasLingkaran
+{
 
     public const phi = 3.14;
     public int $jari;
+
+    public function __construct($isijari = 1){
+        $this->jari = $isijari;
+    }
+
+    public function tampil($nama = 'ban')
+    {
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran {$nama} Hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing()
+    {
+        echo "<br/>";
+        echo "ini dari static: ";
+    }
+
+    public function __destruct(){
+        echo "<br/>";
+        echo "au ah cape: ";
+    }
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 7;
-
-$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
-
-echo "Hasilnya adalah: ". $rumus;
