54e69527e0a095d4462cf96e349abbf8811f2ff9
Rian Agustian <mrian3950@gmail.com>
2025-11-11 06:52:08 +0700

    Ngerapihin Kodingan

--- lingkaran.php
+++ lingkaran.php
@@ -1,31 +1,28 @@
 <?php
 
-namespace App\Math; //namespace
+namespace App\Math;
 
-class LuasLingkaran
-{
+class LuasLingkaran {
 
     public const phi = 3.14;
-    public int $jari;
-
-    public function __construct($isijari = 1){
-        $this->jari = $isijari;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
     }
 
-    public function tampil($nama = 'ban')
-    {
-        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Lingkaran {$nama} Hasilnya adalah: {$rumus}";
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
-    public static function testing()
-    {
+    public static function testing() {
         echo "<br/>";
-        echo "ini dari static: ";
+        echo "ini dari static";
     }
 
-    public function __destruct(){
+    public function __destruct() {
         echo "<br/>";
-        echo "au ah cape: ";
+        echo "udah ah cape";
     }
 }
