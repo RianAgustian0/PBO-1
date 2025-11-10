3f504902d206617919a3ff198618c0b7b6815ec2
Rian Agustian <mrian3950@gmail.com>
2025-11-11 06:59:10 +0700

    merapikan kode pertemuan5

--- lingkaran.php
+++ lingkaran.php
@@ -1,7 +1,6 @@
 <?php
 
-class LuasLingkaran
-{
+class LuasLingkaran{
 
     public const phi = 3.14;
     public int $jari;
@@ -10,14 +9,12 @@ class LuasLingkaran
         $this->jari = $isijari;
     }
 
-    public function tampil($nama = 'ban')
-    {
+    public function tampil($nama = 'ban'){
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
         echo "Lingkaran {$nama} Hasilnya adalah: {$rumus}";
     }
 
-    public static function testing()
-    {
+    public static function testing(){
         echo "<br/>";
         echo "ini dari static: ";
     }
