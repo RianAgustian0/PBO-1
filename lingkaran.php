755cc7a0c034bd78476ec83a38299e9c6454274c
Rian Agustian <mrian3950@gmail.com>
2025-11-11 06:56:21 +0700

    Merapikan kode pertemuan4

--- lingkaran.php
+++ lingkaran.php
@@ -6,14 +6,12 @@ class LuasLingkaran
     public const phi = 3.14;
     public int $jari;
 
-    public function tampil($nama = 'ban')
-    {
+    public function tampil($nama = 'ban'){
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
         echo "Lingkaran {$nama} Hasilnya adalah: {$rumus}";
     }
 
-    public static function testing()
-    {
+    public static function testing(){
         echo "<br/>";
         echo "ini dari static: ";
     }
